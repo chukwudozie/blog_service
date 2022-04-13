d2db0d2beb43bed6ebd046fc3369568e0f5de00b
chukwudozie <emkychuks062@gmail.com>
2022-04-13 15:08:21 +0100

    performing the create blog operation

--- src/main/java/blog/client/BlogClient.java
+++ src/main/java/blog/client/BlogClient.java
@@ -1,5 +1,12 @@
 package blog.client;
 
+import com.proto.blog.Blog;
+import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class BlogClient {
     public static void main(String[] args) {
 
@@ -8,6 +15,24 @@ public class BlogClient {
 
     }
     private  void run(){
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",50054)
+                .usePlaintext().build();
+
+        BlogServiceGrpc.BlogServiceBlockingStub client = BlogServiceGrpc.newBlockingStub(channel);
+
+        Blog blog = Blog.newBuilder()
+                .setTitle("My First Blog")
+                .setContent("Hello! this is my first blog created gRPC and MongoDB")
+                .setDateCreated("13/04/2022")
+                .setAuthorId("Emeka")
+                .build();
+       CreateBlogResponse response =  client.createBlog(CreateBlogRequest.newBuilder()
+                .setBlog(blog)
+                .build());
+        System.out.println("Received a create blog response");
+        System.out.println(response.toString());
+
+        channel.shutdown();
 
     }
 }
--- src/main/java/blog/service/BlogServiceImpl.java
+++ src/main/java/blog/service/BlogServiceImpl.java
@@ -1,7 +1,48 @@
 package blog.service;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.proto.blog.Blog;
 import com.proto.blog.BlogServiceGrpc;
+import com.proto.blog.CreateBlogRequest;
+import com.proto.blog.CreateBlogResponse;
+import io.grpc.stub.StreamObserver;
+import org.bson.Document;
 
 public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
+
+
+    // Set up mongo database
+    private MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+    private MongoDatabase database = mongoClient.getDatabase("mydb");
+    // the collection gets a table in the database named blog
+    private MongoCollection<Document> collection = database.getCollection("blog");
+
+    @Override
+    public void createBlog(CreateBlogRequest request, StreamObserver<CreateBlogResponse> responseObserver) {
+        System.out.println("Received create blog request");
+        Blog newBlog  = request.getBlog();
+        Document doc = new Document("author_id",newBlog.getAuthorId())
+                .append("title",newBlog.getTitle())
+                .append("content", newBlog.getContent())
+                .append("date_created",newBlog.getDateCreated());
+
+        // create a doc in the mongo DB
+        System.out.println("Inserting blog ...");
+        collection.insertOne(doc);
+        // Get the id of the created doc from the Mongo DB
+        String id = doc.getObjectId("_id").toString();
+        System.out.println("Inserted blog: " +id);
+
+        CreateBlogResponse response = CreateBlogResponse.newBuilder()
+                //toBuilder() enables update of an object created with the builder pattern
+                .setBlog(newBlog.toBuilder().setId(id).build())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
 }
